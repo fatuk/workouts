9ca82f30348d1bf0fa8422141faf2ec8d746ca16
fatuk <fatukk@gmail.com>
2014-06-01 11:49:55 +0400

    Fix images

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -7,7 +7,8 @@ module.exports = {
             'js/plugins.min.js': [
                 'bower_components/jquery/dist/jquery.js',
                 'bower_components/WOW/dist/wow.js',
-                'bower_components/jQuery-Open-Carousel/jquery.openCarousel.js'
+                'bower_components/jQuery-Open-Carousel/jquery.openCarousel.js',
+                'bower_components/jquery-numerator/jquery-numerator.js'
             ]
         }
     }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
 $(function() {
     new WOW().init();
+
+    var heartDisplayed, bookDisplayed, creativityDisplayed = false,
+        heartOptions = {
+            duration: 1000,
+            toValue: 30,
+            onStep: function(now, fx) {
+                $('.js-heartNumbers.animated').find('.js-number').append('%');
+                $('.js-heartNumbers.animated').find('.js-indicator').css({
+                    'height': now + '%'
+                });
+            }
+        },
+        bookOptions = {
+            duration: 1000,
+            toValue: 48,
+            onStep: function(now, fx) {
+                $('.js-bookNumbers.animated').find('.js-number').append('%');
+                $('.js-bookNumbers.animated').find('.js-indicator').css({
+                    'height': now + '%'
+                });
+            }
+        },
+        creativityOptions = {
+            duration: 1000,
+            toValue: 75,
+            onStep: function(now, fx) {
+                $('.js-creativityNumbers.animated').find('.js-number').append('%');
+                $('.js-creativityNumbers.animated').find('.js-indicator').css({
+                    'height': now + '%'
+                });
+            }
+        };
+    $(document).on('scroll', function(event) {
+        // heart
+        if ($('.js-heartNumbers.animated').length !== 0 && !heartDisplayed) {
+            heartDisplayed = true;
+            $('.js-heartNumbers.animated').find('.js-number').numerator(heartOptions);
+        }
+        if ($('.js-bookNumbers.animated').length !== 0 && !bookDisplayed) {
+            bookDisplayed = true;
+            setTimeout(function() {
+                $('.js-bookNumbers.animated').find('.js-number').numerator(bookOptions);
+            }, 500);
+        }
+        if ($('.js-creativityNumbers.animated').length !== 0 && !creativityDisplayed) {
+            creativityDisplayed = true;
+            setTimeout(function() {
+                $('.js-creativityNumbers.animated').find('.js-number').numerator(creativityOptions);
+            }, 1000);
+        }
+    });
 });
